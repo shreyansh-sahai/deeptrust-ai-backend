0f46e7643783c81bc2fc105f004d84ce98b3858e
Shreyansh Sahai <shreyansh.sahai@particle41.com>
2025-12-15 18:08:14 +0530

    Fix import

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
-import { InfrastructureModule } from './infrastructure/infrastructure.module';
-import { SyncModule } from './api/sync/sync.module';
+import { InfrastructureModule } from '@infrastructure/infrastructure.module';
+import { SyncModule } from '@api/sync/sync.module';
 import { AuthController } from '@api/controllers/auth/auth.controller';
-import { AuthService } from './application/auth/services/auth.service';
-import { HealthModule } from './api/health/health.module';
+import { AuthService } from '@application/auth/services/auth.service';
+import { HealthModule } from '@api/health/health.module';
 
 @Module({
   imports: [InfrastructureModule, SyncModule, HealthModule],
