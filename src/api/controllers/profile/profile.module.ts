88fec12f32ff1320a7d580d24b9ec7a1d72180bf
Shreyansh Sahai <shreyansh.sahai@particle41.com>
2025-12-22 16:09:14 +0530

    Fix Dependancy Injection

--- src/api/controllers/profile/profile.module.ts
+++ src/api/controllers/profile/profile.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ProfileController } from './profile.controller';
 import { ProfileService } from '@application/profile/services/profile.service';
+import { InfrastructureModule } from '@infrastructure/infrastructure.module';
 
 @Module({
+  imports: [InfrastructureModule],
   controllers: [ProfileController],
   providers: [ProfileService],
 })
